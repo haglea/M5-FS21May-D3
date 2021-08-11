a8b87bb1567cf94ead723a4a49573216091587f4
haglea <haglea@icloud.com>
2021-08-11 16:17:29 +0200

    try catch, middlewares

--- src/services/blogPosts/index.js
+++ src/services/blogPosts/index.js
@@ -13,30 +13,49 @@ const blogPostsRouter = express.Router()
 const getblogPosts = () => JSON.parse(fs.readFileSync(blogPostsJSONPath))
 const writeblogPosts = content => fs.writeFileSync(blogPostsJSONPath, JSON.stringify(content))
 
+const anotherLoggerMiddleware = (request, response, next) => {
+    // route middleware
+    next()
+  }
+
 // GET /blogPosts => returns the list of blogposts
-blogPostsRouter.get("/", (request, response) => {
-    const blogPosts = getblogPosts()
-    response.send(blogPosts)
+blogPostsRouter.get("/", anotherLoggerMiddleware, (request, response, next) => {
+    try {
+        const blogPosts = getblogPosts()
+        response.send(blogPosts)
+    } catch (error) {
+        next(error)
+    }
+    
 })
 
 // GET /blogPosts /123 => returns a single blogpost
-blogPostsRouter.get("/:aID", (request, response) => {
+blogPostsRouter.get("/:aID", (request, response, next) => {
+    try {       
     const blogPosts = getblogPosts()
     const blogPost = blogPosts.find(b => b.id === request.params.aID)
     response.send(blogPost)
+    } catch (error) {
+        next(error)
+    }
 })
 
 // POST /blogPosts => create a new blogpost
-blogPostsRouter.post("/", (request, response) => {
+blogPostsRouter.post("/", (request, response, next) => {
+    try {       
     const newBlogPost = { ...request.body, id: uniqid(), createdAt: new Date()}
     const blogPosts = getblogPosts()
     blogPosts.push(newBlogPost)
     writeblogPosts(blogPosts)
     response.status(201).send({ id: newBlogPost.id })
+    } catch (error) {
+        next(error)
+  }
 })
 
 // PUT /blogPosts /123 => edit the blogpost with the given id
-blogPostsRouter.put("/:aID", (request, response) => {
+blogPostsRouter.put("/:aID", (request, response, next) => {
+    try {       
     const blogPosts = getblogPosts()
     const updatedBlogPost = { ...request.body, id: request.params.aID }
     const remainingBlogPosts = blogPosts.filter(blogPost => blogPost.id !== request.params.aID)
@@ -44,15 +63,22 @@ blogPostsRouter.put("/:aID", (request, response) => {
 
     writeblogPosts(remainingBlogPosts)
     response.send(updatedBlogPost)
+    } catch (error) {
+        next(error)
+    }
 })
 
 // DELETE /blogPosts /123 => delete the blogpost with the given id
-blogPostsRouter.delete("/:aID", (request, response) => {
+blogPostsRouter.delete("/:aID", (request, response, next) => {
+    try {       
     const blogPosts = getblogPosts()
     const remainingBlogPosts = blogPosts.filter(blogPost => blogPost.id !== request.params.aID)
         
     writeblogPosts(remainingBlogPosts)
     response.status(204).send()
+    } catch (error) {
+        next(error)
+    }
 })
 
 export default blogPostsRouter
